306c6dea8447b712077cc84b5f02c67ad2b25a4a
Jessie Wooten <JessieWootenVisual@gmail.com>
2025-04-05 10:49:26 -0500

    chore: add test package dependency

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { add } from "@repo/math/add";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. added: {add(1, 2)}
         </p>
         <a
           className="App-link"
