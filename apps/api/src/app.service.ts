0fd099d459d03d71e3830b53d5e585b63d6e3625
Jessie Wooten <JessieWootenVisual@gmail.com>
2025-04-05 14:50:35 -0500

    refactor: repurpose web app as api

--- apps/api/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { add } from '@repo/math/add';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! ' + add(1, 2);
-  }
-}
